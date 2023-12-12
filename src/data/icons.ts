46d147d1efb3523525eef79f8a21a831247e9ec5
Aurel-Marian Borda <aurelborda@users.noreply.github.com>
2023-12-12 12:03:37 +0000

    Add a list of Font Awesome icons for contact and social links

---
+++ src/data/icons.ts
@@ -0,0 +1,26 @@
+interface Props {
+  [key: string]: string
+}
+
+const props: Props = {
+  behance: 'fa-brands fa-behance',
+  discord: 'fa-brands fa-discord',
+  dribbble: 'fa-brands fa-dribbble',
+  email: 'fa-solid fa-envelope',
+  facebook: 'fa-brands fa-facebook',
+  github: 'fa-brands fa-github',
+  gitlab: 'fa-brands fa-gitlab',
+  instagram: 'fa-brands fa-instagram',
+  linkedin: 'fa-brands fa-linkedin-in',
+  phone: 'fa-solid fa-phone',
+  skype: 'fa-brands fa-skype',
+  stackexchange: 'fa-brands fa-stack-exchange',
+  stackoverflow: 'fa-brands fa-stack-overflow',
+  telegram: 'fa-brands fa-telegram',
+  tiktok: 'fa-brands fa-tiktok',
+  twitch: 'fa-brands fa-twitch',
+  x: 'fa-brands fa-x-twitter',
+  youtube: 'fa-brands fa-youtube',
+}
+
+export default props
